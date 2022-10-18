e321f43674883f96af3e842cbac21ce3aa79fcc8
Catalin Banici <catalinbanici95@yahoo.com>
2022-10-18 18:53:22 +0200

    added first functionality

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+const notificationList = document.querySelectorAll('li');
 
-function setToRead() {
-    
-    const notification = document.querySelectorAll('.notification').forEach(function(element){
-        element.addEventListener('click', function(event){
-            event.target.classList.remove('new-notification');   
-        });
+
+
+notificationList.forEach(function(item){
+    item.children[1].addEventListener('click', function(){
+        // removing the blue-ish background color of the notification li
+        const paraSelector = item.children[1];
+        paraSelector.parentElement.classList.remove('new-notification');
+        // removing the orange dot
+        const iconSelector = item.children[1].children[0].children[2];
+        iconSelector.removeChild(iconSelector.firstElementChild);        
     });
-}
+});
 
-setToRead();
+
+
