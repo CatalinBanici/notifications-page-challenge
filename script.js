20608097ab622871056044449ca84ac4db54f02c
Catalin Banici <catalinbanici95@yahoo.com>
2022-10-20 18:45:41 +0200

    added first functionality 2

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const notificationList = document.querySelectorAll('li');
+let notificationList = document.querySelectorAll('li');
+let markAllAsRead = document.querySelector('.mark-all');
+let notificationNumber = document.querySelector('.notif-number');
+let notificationUnread = document.querySelectorAll('.new-notification');
 
 
+let notifArr = [];
 
-notificationList.forEach(function(item){
-    item.children[1].addEventListener('click', function(){
-        // removing the blue-ish background color of the notification li
-        const paraSelector = item.children[1];
-        paraSelector.parentElement.classList.remove('new-notification');
-        // removing the orange dot
-        const iconSelector = item.children[1].children[0].children[2];
-        iconSelector.removeChild(iconSelector.firstElementChild);        
+function setToRead() {
+    
+    notificationList.forEach(function(item){
+        item.children[1].addEventListener('click', function(){
+            // removing the blue-ish background color of the notification li
+            const paraSelector = item.children[1];
+            paraSelector.parentElement.classList.remove('new-notification');
+            // removing the orange dot
+            const iconSelector = item.children[1].children[0].children[2];
+            iconSelector.removeChild(iconSelector.firstElementChild);  
+            
+            // updating the counter           
+            const classN = item.classList[1];
+            
+            notifArr.push(classN);
+                    
+            notificationNumber.innerHTML = notifArr.length;
+            
+        });
+  
     });
-});
+              
+}
+setToRead();
+
+
+
+
 
 
 
