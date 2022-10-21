4330d54bc0a43d490de3d069439a7cde670458c7
Catalin Banici <catalinbanici95@yahoo.com>
2022-10-21 19:59:31 +0200

    last commit

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let notificationList = document.querySelectorAll('li');
-let markAllAsRead = document.querySelector('.mark-all');
-let notificationNumber = document.querySelector('.notif-number');
-let notificationUnread = document.querySelectorAll('.new-notification');
-
-
-let notifArr = [];
+const notificationList = document.querySelectorAll('li');
+const markAll = document.querySelector('.mark-all');
+const notificationCounter = document.querySelector('.notif-number');
 
 function setToRead() {
     
@@ -13,24 +9,39 @@ function setToRead() {
             // removing the blue-ish background color of the notification li
             const paraSelector = item.children[1];
             paraSelector.parentElement.classList.remove('new-notification');
+
             // removing the orange dot
             const iconSelector = item.children[1].children[0].children[2];
-            iconSelector.removeChild(iconSelector.firstElementChild);  
-            
-            // updating the counter           
-            const classN = item.classList[1];
-            
-            notifArr.push(classN);
-                    
-            notificationNumber.innerHTML = notifArr.length;
-            
-        });
-  
-    });
-              
-}
-setToRead();
+            iconSelector.removeChild(iconSelector.firstElementChild);   
 
+            // updating the counter           
+            notificationCounter.innerHTML--                
+        });              
+    }); 
+       
+}
+
+function markAllAsRead() {
+
+    notificationList.forEach(function(item){
+        markAll.addEventListener('click', function(){
+            // removing the blue-ish background color of the notification li
+            const paraSelector = item.children[1];
+            paraSelector.parentElement.classList.remove('new-notification');
+
+            // removing the orange dot
+            const iconSelector = item.children[1].children[0].children[2];
+            iconSelector.removeChild(iconSelector.firstElementChild);   
+
+            // updating the counter           
+            notificationCounter.innerHTML--                
+        });              
+    }); 
+    
+}
+
+setToRead();
+markAllAsRead();
 
 
 
